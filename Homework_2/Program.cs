4763871b8230b09f4c1227d1263dce2a9607ead5
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-05 14:43:51 +0400

    Задача 3 готова

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -15,7 +15,7 @@ SecondDigit(num);
 
 //Задача 2. Напишите программу, которая выводит третью цифру
 //заданного числа или сообщает, что третьей цифры нет.
-
+/*
 int ThirdDigit (int num)
 {
    while (num >= 1000) num = num/10;
@@ -34,3 +34,21 @@ else
 Console.WriteLine($"Третья цифра введенного вами числа {num} - это ");
 Console.WriteLine (ThirdDigit (num));
 }
+*/
+
+//Задача 3.Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет,является ли этот день выходным.
+
+bool Weekend(int numday)
+{
+    if(numday >= 6 && numday <= 7) return true;
+    else return false;
+}
+
+Console.WriteLine("Введите цифру, обозначающую день недели, чтобы узнать, является ли он выходным: ");
+int numday = Convert.ToInt32(Console.ReadLine());
+
+if(Weekend(numday) == true) 
+Console.WriteLine("Да, день является выходным");
+else 
+Console.WriteLine("День не является выходным");

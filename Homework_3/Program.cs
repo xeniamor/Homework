6f75d6535137c6b94bb00b138329c00e684e1abb
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-05 15:23:31 +0400

    Задача 1

---
+++ Homework_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход пятизначное число
+//и проверяет, является ли оно палиндромом(решение для универсального числа!)
+
+bool Palindrome(int num)
+ {
+      int num1 = num;
+      int reversNum = 0;
+        
+     while(num1 > 0)
+     {
+         int lastDigit = num1 % 10;
+         reversNum = reversNum * 10 + lastDigit;
+         num1 = num1 / 10;
+     }
+
+     if(num == reversNum) return true;
+     else return false;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(Palindrome(num) == true)
+Console.WriteLine("Число является палиндромом");
+else 
+Console.WriteLine("Число не является палиндромом");

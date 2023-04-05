1276b38aa4f46ac5ba25ad093fcc75a1ba6610f2
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-05 13:54:55 +0400

    Задача 1 готова

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,4 +1,13 @@
 ﻿//Задача 1. Напишите программу, которая принимает на вход трёхзначное число
 //и на выходе показывает вторую цифру этого числа.
 
+void SecondDigit(int num)
+{
+   int secondNum = num % 100 / 10;
+   Console.WriteLine($"Вторая цифра введенного Вами числа {num} - это {secondNum}");
+}
 
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+SecondDigit(num);

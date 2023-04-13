be8b8bc2ad4ae7ac88a7eaa60c82b247ceeeb784
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-13 09:56:39 +0400

    Задача 2 домашки №5

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 1. Задайте массив заполненный случайными положительными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве.
-
+/*
 int[] CreateArrayPositiveThreeDigitRandom(int size)
 {
    int[] array = new int[size];
@@ -29,4 +29,40 @@ int[] newArray = CreateArrayPositiveThreeDigitRandom(size);
 ShowArray(newArray);
 int result = QuantityEvenNumbers(newArray);
 Console.WriteLine($"Количество четных чисел в массиве - {result}");
+*/
 
+//Задача 2.Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+   int[] array = new int[size];
+   for (int i = 0; i < size; i++)
+      array[i] = new Random().Next(minValue, maxValue + 1);
+   return array;
+}
+
+void ShowArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      Console.Write(array[i] + " ");
+   Console.WriteLine();
+}
+
+int SumElementsOddPosition(int[] array)
+{
+   int sum = 0;
+   for (int i = 1; i < array.Length; i += 2) sum += array[i];
+   return sum;
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(size, min, max);
+ShowArray(newArray);
+int result = SumElementsOddPosition(newArray);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях - {result}");

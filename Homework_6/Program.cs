bde65481b2831b2be48867df0f1a8ed993c7448b
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-13 15:31:49 +0400

    Задача 1 домашки №6

--- Homework_6/Program.cs
+++ Homework_6/Program.cs
@@ -1,3 +1,28 @@
 ﻿//Задача 1. Пользователь вводит с клавиатуры M чисел.
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
+int PositiveNumbers(int[] array)
+{
+   int total = 0;
+
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] > 0)
+         total = total + 1;
+   }
+   return total;
+}
+
+Console.Write("Укажите количество чисел, которое Вы хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+   Console.Write($"Введите {i + 1}-е число: ");
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+int result = PositiveNumbers(array);
+Console.Write($"{result} чисел из введенных больше 0.");
+

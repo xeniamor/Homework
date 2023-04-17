23675e6164369437deafb9898b1bfae87b7c9777
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-17 11:01:51 +0400

    Задача №2 домашки 4

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -16,4 +16,26 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.Write($"Число {a} в степени {b} равняется - {NumDegree(a, b)}");
 */
 
+//Задача 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int SummDigit(int num)
+{
+   int num1 = num;
+   int sumDigit = 0;
+   
+    while(num1 > 0)
+   {
+        int lastDigit = num1 % 10;
+        sumDigit = sumDigit + lastDigit;
+        num1 = num1 / 10;
+   }
+   return sumDigit;
+}
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = SummDigit(a);
+Console.WriteLine($"Сумма цифр введенного числа {a} равняется - {result}");
+*/
+
+
 

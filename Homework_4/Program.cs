f16ea5c3e71bd8cf4f8c7a29009bc88d3e253fcd
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-17 10:54:28 +0400

    Задача №1 домашки 4

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B.
+/*
+double NumDegree(double a, int b)
+{
+   double result = 1;
+   for (int i = 1; i <= b; i += 1)
+      result = result * a;
+   return result;
+}
+Console.Write("Введи число а, которое будем возводить в степень: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите натуральное число b, которое укажет на степень, в которую необходимо возвести число а: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число {a} в степени {b} равняется - {NumDegree(a, b)}");
+*/
+
 

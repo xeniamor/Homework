c566c1cfd4065a7aa965e51b67a82bef56b21fb1
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-26 11:05:38 +0400

    Задача №3 домашки 8

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -63,8 +63,9 @@ Show2dArray(myArray);
 
 // Задача 2. Задайте прямоугольный двумерный массив. Напишите программу,
 // которая будет находить строку с наименьшей суммой элементов.
-
+/*
 int[,] CreateRandom2dArray()
+
 {
    Console.WriteLine("Введите корректное количество строк:");
    int rows = Convert.ToInt32(Console.ReadLine());
@@ -101,27 +102,108 @@ void Show2dArray(int[,] array)
 
 void RowMinSum(int[,] array)
 {
-    int minRow = 0;
-       for (int i = 0; i < array.GetLength(1); i++)
-    {
-        minRow += array[0, i];
-    }
-    
-    int minSumRow = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        int sumRow = 0;
-        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
-        if (sumRow < minRow)
-        {
-            minRow = sumRow;
-            minSumRow = i;
-        }
-        sumRow = 0;
-    }
-    Console.Write($"Cтрока c минимальной суммой элементов - {minSumRow + 1}");
+   int minRow = 0;
+   for (int i = 0; i < array.GetLength(1); i++)
+   {
+      minRow += array[0, i];
+   }
+
+   int minSumRow = 0;
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      int sumRow = 0;
+      for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+      if (sumRow < minRow)
+      {
+         minRow = sumRow;
+         minSumRow = i;
+      }
+      sumRow = 0;
+   }
+   Console.Write($"Cтрока c минимальной суммой элементов - {minSumRow + 1}");
 }
 
 int[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
 RowMinSum(myArray);
+*/
+//  Задача 3.Задайте две матрицы. Напишите программу, 
+//  которая будет находить произведение двух матриц.
+
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+
+        }
+    }
+    return array;
+}
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MultiplicationMatrix(int[,] array1, int[,] array2)
+{
+    int[,] array = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int k = 0; k < array1.GetLength(1); k++)
+    {
+        for (int i = 0; i < array1.GetLength(0); i++)
+        {
+            for (int j = 0; j < array2.GetLength(1); j++)
+            {
+                array[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return array;
+}
+Console.Write("Введите корректное количество строк первой матрицы: ");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите корректное количество столбцов первой матрицы: ");
+int columns1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение первой матрицы: ");
+int minValue1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение первой матрицы: ");
+int maxValue1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите корректное количество строк второй матрицы: ");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите корректное количество столбцов второй матрицы: ");
+int columns2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение второй матрицы: ");
+int minValue2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение второй матрицы: ");
+int maxValue2 = Convert.ToInt32(Console.ReadLine());
+
+while (columns1 != rows2)
+{
+    Console.WriteLine("Некорректно введенные данные! Матрицы не могут быть перемножены!");
+    Console.WriteLine("Количество столбцов первой матрицы, должно быть равно количеству строк второй матрицы!");
+    Console.Write("Введите корректное количество столбцов первой матрицы: ");
+    columns1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите корректное количество строк второй матрицы: ");
+    rows2 = Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] Matrix1 = CreateRandom2dArray(rows1, columns1, minValue1, maxValue1);
+int[,] Matrix2 = CreateRandom2dArray(rows2, columns2, minValue2, maxValue2);
+int[,] MatrixRes = MultiplicationMatrix(Matrix1, Matrix2);
+
+
+Show2dArray(Matrix1);
+Show2dArray(Matrix2);
+Show2dArray(MatrixRes);

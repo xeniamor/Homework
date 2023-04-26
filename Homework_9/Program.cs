1e1cb3a88664319e740129d20246dc1dc1d2a4a8
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-26 11:45:38 +0400

    Задача №1 домашки 9

---
+++ Homework_9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1.Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+void ShowNum(int num)
+{
+    Console.Write(num + " ");
+    if(num > 1) ShowNum(num - 1);
+}
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ShowNum(number);

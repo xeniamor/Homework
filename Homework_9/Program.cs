f101399a44689125e6c27919e458e11ac226b7ae
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-26 11:50:17 +0400

    Задача №2 домашки 9

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 1.Задайте значение N. Напишите программу,
 // которая выведет все натуральные числа в промежутке от N до 1.
 // Выполнить с помощью рекурсии.
-
+/*
 void ShowNum(int num)
 {
     Console.Write(num + " ");
@@ -11,3 +11,21 @@ Console.Write("Введите число N: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 ShowNum(number);
+*/
+
+// Задача 2. Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+ int SumNaturalElements(int m, int n)
+{
+    if(m == n) return m;
+    else return SumNaturalElements(m + 1, n) + m;    
+}
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNaturalElements(m, n);
+Console.Write($"Сумма натуральных элементов между числами {m} и {n} равняется - {result}");

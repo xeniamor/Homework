a6fa897ad7ef8d8e93a24f8866df0b0538e1f8c7
Xenia Morozova <15.ks.love.ru@mail.ru>
2023-04-26 11:55:40 +0400

    Задача №3 домашки 9

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -15,7 +15,7 @@ ShowNum(number);
 
 // Задача 2. Задайте значения M и N. Напишите программу,
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
-
+/*
  int SumNaturalElements(int m, int n)
 {
     if(m == n) return m;
@@ -29,3 +29,25 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 int result = SumNaturalElements(m, n);
 Console.Write($"Сумма натуральных элементов между числами {m} и {n} равняется - {result}");
+*/
+
+// Задача 3. Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+int AkkermanFunction(int m, int n)
+{
+    if(m == 0) 
+        return n + 1;
+    else if(n == 0 && m > 0) 
+        return AkkermanFunction(m - 1, 1);
+    else 
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+Console.Write("Введите первое неотрицательное целое число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе неотрицательное целое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m, n);
+Console.Write($"Значение вычисления функции Аккермана равняется - {AkkermanFunction(m, n)}.");
